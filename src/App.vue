b71f24738b746cd0def2430394105e4b603dc5f1
ShuvoMallick01 <shuvacse01@gmail.com>
2023-08-25 12:53:15 +0600

    Job Details page Update

--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,7 @@
     >
 
     <div
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-8 md:gap-8 lg:gap-16"
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-8 md:gap-8 lg:gap-10 xl:gap-28"
     >
       <div
         class="col-span-1 md:col-span-1 lg:col-span-2 order-2 md:order-1 mt-10 md:mt-0"
@@ -88,7 +88,7 @@
         </p>
 
         <!-- Social Media -->
-        <div class="flex gap-2 justify-end mt-5">
+        <div class="flex gap-2 justify-end mt-8 pb-10">
           <p class="description-text">Share Job</p>
           <div class="social-icon-body">
             <p class="absolute text-sm">
@@ -114,9 +114,146 @@
             </p>
           </div>
         </div>
+
+        <!-- Related Jobs -->
+        <div class="mt-6 pt-6 border-t border-slate-300 py-6">
+          <h1 class="heading pt-2 mb-8">Related Jobs</h1>
+
+          <!-- Cards -->
+          <div class="border-t border-slate-300 py-6">
+            <!-- card -->
+            <div
+              class="md:flex justify-between border-b pb-5 mb-5 items-center"
+            >
+              <div class="flex items-center gap-4">
+                <div
+                  class="bg-secondary w-8 h-8 md:w-10 md:h-10 text-slate-100 text-base md:text-xl rounded-full flex justify-center relative items-center"
+                >
+                  <p class="absolute">P</p>
+                </div>
+
+                <div class="">
+                  <p class="text-slate-500 text-xs md:text-sm mb-1">
+                    robinhood
+                  </p>
+                  <h4
+                    class="font-semibold text-slate-600 mb-2 md:text-lg text-sm"
+                  >
+                    Senior Site Reliability Engineer
+                  </h4>
+
+                  <div class="flex gap-2">
+                    <button
+                      class="text-xs bg-slate-200 text-slate-500 py-1 px-1 rounded"
+                    >
+                      Full Time
+                    </button>
+                    <button
+                      class="text-xs bg-slate-200 text-slate-500 py-1 px-1 rounded"
+                    >
+                      Remote
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div class="flex items-center gap-2 ms-14 md:ms-0 md:mt-0 mt-4">
+                <button class="btn-sm">Apply Now</button>
+
+                <p class="text-xs text-slate-400">3hr</p>
+              </div>
+            </div>
+
+            <!-- card -->
+            <div
+              class="md:flex justify-between border-b pb-5 mb-5 items-center"
+            >
+              <div class="flex items-center gap-4">
+                <div
+                  class="bg-secondary w-8 h-8 md:w-10 md:h-10 text-slate-100 text-base md:text-xl rounded-full flex justify-center relative items-center"
+                >
+                  <p class="absolute">P</p>
+                </div>
+
+                <div class="">
+                  <p class="text-slate-500 text-xs md:text-sm mb-1">
+                    robinhood
+                  </p>
+                  <h4
+                    class="font-semibold text-slate-600 mb-2 md:text-lg text-sm"
+                  >
+                    Senior Site Reliability Engineer
+                  </h4>
+
+                  <div class="flex gap-2">
+                    <button
+                      class="text-xs bg-slate-200 text-slate-500 py-1 px-1 rounded"
+                    >
+                      Full Time
+                    </button>
+                    <button
+                      class="text-xs bg-slate-200 text-slate-500 py-1 px-1 rounded"
+                    >
+                      Remote
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div class="flex items-center gap-2 ms-14 md:ms-0 md:mt-0 mt-4">
+                <button class="btn-sm">Apply Now</button>
+
+                <p class="text-xs text-slate-400">3hr</p>
+              </div>
+            </div>
+
+            <!-- card -->
+            <div
+              class="md:flex justify-between border-b pb-5 mb-5 items-center"
+            >
+              <div class="flex items-center gap-4">
+                <div
+                  class="bg-secondary w-8 h-8 md:w-10 md:h-10 text-slate-100 text-base md:text-xl rounded-full flex justify-center relative items-center"
+                >
+                  <p class="absolute">P</p>
+                </div>
+
+                <div class="">
+                  <p class="text-slate-500 text-xs md:text-sm mb-1">
+                    robinhood
+                  </p>
+                  <h4
+                    class="font-semibold text-slate-600 mb-2 md:text-lg text-sm"
+                  >
+                    Senior Site Reliability Engineer
+                  </h4>
+
+                  <div class="flex gap-2">
+                    <button
+                      class="text-xs bg-slate-200 text-slate-500 py-1 px-1 rounded"
+                    >
+                      Full Time
+                    </button>
+                    <button
+                      class="text-xs bg-slate-200 text-slate-500 py-1 px-1 rounded"
+                    >
+                      Remote
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div class="flex items-center gap-2 ms-14 md:ms-0 md:mt-0 mt-4">
+                <button class="btn-sm">Apply Now</button>
+
+                <p class="text-xs text-slate-400">3hr</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <!-- card -->
+      <!-- Right Company Details -->
       <div class="order-1 md:order-2">
         <div class="bg-gray-200/50 border border-slate-300 p-6 rounded-lg">
           <div class="text-center">
@@ -143,88 +280,6 @@
             <a class="btn btn-primary">Apply Now</a>
             <a class="btn btn-outline-primary">Visit Website</a>
           </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- Related Jobs -->
-  <section class="myContainer mt-6 pt-6">
-    <h1 class="heading pt-2 mb-8">Related Jobs</h1>
-
-    <!-- Job Card -->
-    <div class="col-span-2 border-t border-slate-300 py-6 lg:px-10 px-5">
-      <!-- card -->
-      <div class="md:flex justify-between border-b pb-5 mb-5 items-center">
-        <div class="flex items-center gap-4">
-          <div
-            class="bg-secondary w-8 h-8 md:w-10 md:h-10 text-slate-100 text-base md:text-xl rounded-full flex justify-center relative items-center"
-          >
-            <p class="absolute">P</p>
-          </div>
-
-          <div class="">
-            <p class="text-slate-500 text-xs md:text-sm mb-1">robinhood</p>
-            <h4 class="font-semibold text-slate-600 mb-2 md:text-lg text-sm">
-              Senior Site Reliability Engineer
-            </h4>
-
-            <div class="flex gap-2">
-              <button
-                class="text-xs bg-slate-200 text-slate-500 py-1 px-1 rounded"
-              >
-                Full Time
-              </button>
-              <button
-                class="text-xs bg-slate-200 text-slate-500 py-1 px-1 rounded"
-              >
-                Remote
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div class="flex items-center gap-2 ms-14 md:ms-0 md:mt-0 mt-4">
-          <button class="btn-sm">Apply Now</button>
-
-          <p class="text-xs text-slate-400">3hr</p>
-        </div>
-      </div>
-
-      <!-- card -->
-      <div class="md:flex justify-between border-b pb-5 mb-5 items-center">
-        <div class="flex items-center gap-4">
-          <div
-            class="bg-secondary w-8 h-8 md:w-10 md:h-10 text-slate-100 text-base md:text-xl rounded-full flex justify-center relative items-center"
-          >
-            <p class="absolute">P</p>
-          </div>
-
-          <div class="">
-            <p class="text-slate-500 text-xs md:text-sm mb-1">robinhood</p>
-            <h4 class="font-semibold text-slate-600 mb-2 md:text-lg text-sm">
-              Senior Site Reliability Engineer
-            </h4>
-
-            <div class="flex gap-2">
-              <button
-                class="text-xs bg-slate-200 text-slate-500 py-1 px-1 rounded"
-              >
-                Full Time
-              </button>
-              <button
-                class="text-xs bg-slate-200 text-slate-500 py-1 px-1 rounded"
-              >
-                Remote
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div class="flex items-center gap-2 ms-14 md:ms-0 md:mt-0 mt-4">
-          <button class="btn-sm">Apply Now</button>
-
-          <p class="text-xs text-slate-400">3hr</p>
         </div>
       </div>
     </div>
