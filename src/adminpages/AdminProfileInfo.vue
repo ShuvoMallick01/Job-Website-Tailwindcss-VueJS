c5b19344fe114cfd24946f340a19dd6592814a78
ShuvoMallick01 <shuvacse01@gmail.com>
2023-10-03 17:16:03 +0600

    Complete Admin Profile information

--- src/adminpages/AdminProfileInfo.vue
+++ src/adminpages/AdminProfileInfo.vue
@@ -1,134 +1,44 @@
 <template>
   <h2 class="heading-paragraph mb-5 border-b pb-4 uppercase">
-    BASIC INFORMATION
+    COMPANY PROFILE
   </h2>
 
   <form>
     <div class="md:grid grid-cols-2 gap-5">
       <div class="mb-6">
-        <!-- Name -->
+        <!-- company name -->
         <label
-          for="name"
+          for="companyname"
           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >Your Name:</label
+          >Company Name (optional):</label
         >
         <input
           type="text"
-          id="name"
+          id="companyname"
           class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="Ex.: Shuvo Mallick, John Smit"
-          required
+          placeholder="Ex.: Bit Skyber, Facebook"
         />
       </div>
 
-      <!-- Profession -->
+      <!-- email -->
       <div class="mb-6">
         <label
-          for="profession"
+          for="companyemail"
           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >Professional title:</label
+          >Email:</label
         >
         <input
-          type="text"
-          id="profession"
+          type="email"
+          id="companyemail"
           class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="Ex.: Web Developer"
+          placeholder="Ex.: company@domain.com"
           required
         />
       </div>
     </div>
 
     <div class="md:grid grid-cols-2 gap-5">
-      <!-- Language -->
-      <div class="mb-6">
-        <label
-          for="language"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >Language:</label
-        >
-        <input
-          type="text"
-          id="language"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="Ex.: Web Developer"
-          required
-        />
-      </div>
-
-      <!-- Age -->
-      <div class="mb-6">
-        <label
-          for="age"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >Age:</label
-        >
-        <input
-          type="text"
-          id="age"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="Ex.: 30, 28"
-          required
-        />
-      </div>
-    </div>
-
-    <div class="md:grid grid-cols-2 gap-5">
-      <!-- Current Salary ($) -->
-      <div class="mb-6">
-        <label
-          for="currentsalary"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >Current Salary ($):</label
-        >
-        <input
-          type="text"
-          id="currentsalary"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="Ex.: $2000"
-          required
-        />
-      </div>
-
-      <!-- Expected Salary -->
-      <div class="mb-6">
-        <label
-          for="expectedsalary"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >Expected Salary:</label
-        >
-        <input
-          type="text"
-          id="expectedsalary"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="Ex.: $3500"
-          required
-        />
-      </div>
-    </div>
-
-    <!-- Description -->
-    <div class="mb-6">
-      <label
-        for="description"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >Description:</label
-      >
-      <textarea
-        type="text"
-        rows="4"
-        id="description"
-        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-        placeholder="About Your Self .."
-        required
-      />
-    </div>
-
-    <h2 class="heading-paragraph mb-5 border-b pb-4 mt-10 uppercase">
-      CONTACT INFORMATION
-    </h2>
-
-    <div class="md:grid grid-cols-2 gap-5">
-      <!-- Current Salary ($) -->
+      <!-- phone -->
       <div class="mb-6">
         <label
           for="phone"
@@ -139,30 +49,125 @@
           type="number"
           id="phone"
           class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="Ex.: +1 123 450 7890"
+          placeholder="Ex.: +88 0123456789"
           required
         />
       </div>
 
-      <!-- Email -->
+      <!-- website -->
       <div class="mb-6">
         <label
-          for="email"
+          for="website"
           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >Email Address</label
+          >Website:</label
         >
         <input
-          type="email"
-          id="email"
+          type="text"
+          id="website"
           class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="name@flowbite.com"
+          placeholder="Ex.: www.name.com"
           required
         />
       </div>
     </div>
 
     <div class="md:grid grid-cols-2 gap-5">
-      <!-- Country -->
+      <!-- Est. Since -->
+      <div class="mb-6">
+        <label
+          for="estsince"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Est. Since:</label
+        >
+        <input
+          type="number"
+          id="estsince"
+          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          placeholder=""
+          required
+        />
+      </div>
+
+      <!-- Team Size -->
+      <div class="mb-6">
+        <label
+          for="teamsize"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Team Size:</label
+        >
+        <input
+          type="number"
+          id="teamsize"
+          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          placeholder=""
+          required
+        />
+      </div>
+    </div>
+
+    <!-- about company -->
+    <div class="mb-6">
+      <label
+        for="aboutcompany"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >About Company:</label
+      >
+      <textarea
+        type="text"
+        rows="4"
+        id="aboutcompany"
+        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+        placeholder="write about your company .."
+        required
+      />
+    </div>
+
+    <!-- SOCIAL NETWORK -->
+    <h2 class="heading-paragraph mb-5 border-b pb-4 mt-10 uppercase">
+      SOCIAL NETWORK
+    </h2>
+
+    <div class="md:grid grid-cols-2 gap-5">
+      <!-- FACEBOOK -->
+      <div class="mb-6">
+        <label
+          for="facebook"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Facebook:</label
+        >
+        <input
+          type="text"
+          id="facebook"
+          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          placeholder=""
+          required
+        />
+      </div>
+
+      <!-- Linkdin -->
+      <div class="mb-6">
+        <label
+          for="linkedin"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Twitter:</label
+        >
+        <input
+          type="text"
+          id="linkedin"
+          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          placeholder=""
+          required
+        />
+      </div>
+    </div>
+
+    <!-- CONTACT INFORMATION -->
+    <h2 class="heading-paragraph mb-5 border-b pb-4 mt-10 uppercase">
+      CONTACT INFORMATION
+    </h2>
+
+    <div class="md:grid grid-cols-2 gap-5">
+      <!-- country -->
       <div class="mb-6">
         <label
           for="country"
@@ -178,25 +183,7 @@
         />
       </div>
 
-      <!-- Postcode -->
-      <div class="mb-6">
-        <label
-          for="postcode"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >Postcode</label
-        >
-        <input
-          type="number"
-          id="postcode"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder=""
-          required
-        />
-      </div>
-    </div>
-
-    <div class="md:grid grid-cols-2 gap-5">
-      <!-- City -->
+      <!-- city -->
       <div class="mb-6">
         <label
           for="city"
@@ -211,30 +198,25 @@
           required
         />
       </div>
-
-      <!-- Full Address -->
-      <div class="mb-6">
-        <label
-          for="fulladdress"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >Full Address</label
-        >
-        <input
-          type="text"
-          id="fulladdress"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder=""
-          required
-        />
-      </div>
     </div>
 
-    <button
-      type="submit"
-      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-    >
-      Save Setting
-    </button>
+    <!-- Complete Address -->
+    <div class="mb-6">
+      <label
+        for="completeaddress"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >Complete Address:</label
+      >
+      <input
+        type="text"
+        id="completeaddress"
+        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+        placeholder=""
+        required
+      />
+    </div>
+
+    <button type="submit" class="btn btn-primary px-16">Save</button>
   </form>
 </template>
 
